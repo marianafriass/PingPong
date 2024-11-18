b667361dfd64b12187849bcde6b0a62f33f1f00e
marianafriass <a01709125@tec.mx>
2024-11-17 22:35:29 -0600

    Update Game.h

--- Game.h
+++ Game.h
@@ -2,22 +2,28 @@
 #define GAME_H
 
 #include "Ball.h"
-#include "Boundary.h"
-#include "Players.h"
 #include "Paddle.h"
+#include "Players.h"
+#include "Boundary.h"
 
 class Game {
 private:
     bool isRunning;
+    Ball ball1;          // Composición: La clase contiene un objeto Ball
+    Paddle paddle1;      // Composición: La clase contiene un objeto Paddle
+    Players player1;     // Composición: La clase contiene un objeto Players
+    Boundary boundary1;  // Composición: La clase contiene un objeto Boundary
 
 public:
+    // Constructor
     Game();
 
+    // Métodos
     void startGame();
     void updateGame();
     void endGame();
-    void detectCollision(const Ball &ball);
-    void updateScore(Players &player);
+    void detectCollision(Ball ball);
+    void updateScore(Players& player);
     void resetGame();
 };
 
