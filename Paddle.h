c86592b351570f1a2368a8bc900574c1e1d1f102
marianafriass <a01709125@tec.mx>
2024-11-17 20:40:13 -0600

    Update Paddle.h

--- Paddle.h
+++ Paddle.h
@@ -1,20 +1,22 @@
 #ifndef PADDLE_H
 #define PADDLE_H
 
-#include <utility>
+#include "GameObject.h"
 
-class Paddle {
+class Paddle : public GameObject {
 private:
-    std::pair<int, int> position;
     int size;
     int speed;
 
 public:
-    Paddle(int x, int y, int s, int spd);
+    Paddle(int x, int y, int s, int sp) 
+        : GameObject(x, y), size(s), speed(sp) {}
 
-    void moveUp();
-    void moveDown();
-    void draw() const; 
+    void moveUp() { position.second -= speed; }
+    void moveDown() { position.second += speed; }
+
+    void move() override {
+    }
 };
 
-#endif
+#endif 
